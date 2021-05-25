d6927d650b49c1c5c94c6f7f4a030f3acbb9ce8a
JackieBinya <jacquelinebinya@gmail.com>
2021-05-25 10:58:06 +0200

    Adds a native https node server

---
+++ index.js
@@ -0,0 +1,14 @@
+const https = require('https');
+
+const url = `https://www.omdbapi.com/?t=kill&apikey=9ae318ac`
+
+https.get(url, res => {
+    let data = ""
+
+    res.on("data", chunk => {
+      data += chunk;
+    });
+     
+
+    res.on("end", () => console.log(JSON.parse(data)))
+})
